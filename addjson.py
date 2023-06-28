acae2dd3db3644f7b78d28715a3070d160317f53
Universatorium <matthias.bandholtz@google.com>
2023-06-28 22:33:16 +0200

    finale Version

---
+++ addjson.py
@@ -0,0 +1,16 @@
+import json
+
+# JSON-Datei öffnen und Daten laden
+with open('Pokemon.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# Zusätzliche Information hinzufügen
+i=1
+for i, entry in enumerate(data, start=1):
+    entry['ID'] = i
+    bildname = f"{i:03}.png"
+    entry['Bild'] = bildname
+
+# Aktualisierte Daten in die JSON-Datei schreiben
+with open('Pokemon.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)
